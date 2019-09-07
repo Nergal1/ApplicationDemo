0cb423cfcfd5b158bfb192f522f172fa29928e32
zhangchen <dj009717zhangchen@djbx.com>
2019-09-07 14:26:01 +0800

    注释

--- app/src/main/java/com/anbang/myapplication/MainActivity.java
+++ app/src/main/java/com/anbang/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //  设置View
         setContentView(R.layout.activity_main);
     }
 }
